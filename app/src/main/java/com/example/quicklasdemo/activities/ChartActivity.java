c8656332a5e9fae8835b66905e5c8378fedc97f3
mattj <mattjroberson@gmail.com>
2021-04-24 21:30:07 -0500

    Chart Activity: Broke some things into methods. Fixed the crash bug that was due to different depth names.

--- app/src/main/java/com/example/quicklasdemo/activities/ChartActivity.java
+++ app/src/main/java/com/example/quicklasdemo/activities/ChartActivity.java
@@ -3,6 +3,7 @@ package com.example.quicklasdemo.activities;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quicklasdemo.DatabaseHelper;
 import com.example.quicklasdemo.R;
+import com.example.quicklasdemo.data.Curve;
 import com.example.quicklasdemo.data.Track;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
@@ -25,13 +27,13 @@ import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class ChartActivity extends AppCompatActivity {
 
     private static final String TAG = "ChartActivity";
+    private static final String[] DEPTH_NAMES =  {"DEPTH", "DEPT", "Depth", "Dept"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,89 +61,117 @@ public class ChartActivity extends AppCompatActivity {
             mCharts[2] = (LineChart) findViewById(R.id.chart3);
         }
 
-        String Depth; // Initalizing Depth for Different LAS files
-        if (lasData.containsKey("DEPT")) {
-            Depth = "DEPT";
-        } else {
-            Depth = "DEPTH";
-        }
+        String Depth = GetDepthName(lasData);
 
-        for (int i = 0; i < num_curves; i++) {
-            ArrayList<Entry> yVals = new ArrayList<>();
-            for (int x = 0; x < lasData.get(tracks.get(0).component2().get(i).component1()).size(); x++) {
-                yVals.add(new Entry(lasData.get(tracks.get(0).component2().get(0).component1()).get(x), lasData.get(Depth).get(x)));
+        //Track Properties
+        Track track = tracks.get(0);
+
+        Boolean showGrid = track.component3();
+        Boolean isLinear = track.component4();
+        int verticalDivCount = track.component5();
+        int horizontalDivHeight = track.component6();
+
+        //Curve Properties
+        List<Curve> curveList = track.component2();
+        List<Float> depth = lasData.get(Depth);
+        Log.i("TEST", String.valueOf(depth.size()));
+
+        for (int curveIndex = 0; curveIndex < curveList.size(); curveIndex++) {
+            ArrayList<Entry> points = new ArrayList<>();
+            Curve curve = curveList.get(curveIndex);
+
+            String curveName = curve.component1();
+            List<Float> curvePoints = lasData.get(curveName);
+
+            for (int x = 0; x < curvePoints.size(); x++) {
+                Entry point = new Entry(curvePoints.get(x),
+                        depth.get(x));
+                points.add(point);
             }
 
-            float num_values = lasData.get(tracks.get(0).component2().get(i).component1()).size();
+            //float num_values = lasData.get(tracks.get(0).component2().get(i).component1()).size();
 
-            String curveName = tracks.get(0).component2().get(i).component1();
+            String lineStyle = curve.component2();
+            String curveColor = curve.component3();
+            float scaleMin = curve.component4();
+            float scaleMax = curve.component5();
 
-            String lineStyle = tracks.get(0).component2().get(i).component2();
-            String curveColor = tracks.get(0).component2().get(i).component3();
-            float scaleMin = tracks.get(0).component2().get(i).component4();
-            float scaleMax = tracks.get(0).component2().get(i).component5();
+            LineDataSet set1 = new LineDataSet(points, curveName);
+            SetCurveStyle(set1, lineStyle);
+            SetCurveColor(set1, curveColor);
 
-            LineDataSet set1 = new LineDataSet(yVals, curveName);
-
-            switch (lineStyle) {
-                case "Normal":
-                    set1.setLineWidth(1f);
-                    break;
-                case "Dotted":
-                    set1.enableDashedLine(2f, 2f, 2f);
-                    break;
-                case "Bold":
-                    set1.setLineWidth(2f);
-                    break;
-            }
-
-            System.out.println(curveColor);
-
-            switch (curveColor) {
-                case "FF0000":
-                    set1.setColor(Color.RED);
-                    break;
-                case "0000FF":
-                    set1.setColor(Color.BLUE);
-                    break;
-                case "00FF00":
-                    set1.setColor(Color.GREEN);
-                    break;
-            }
-
-            Boolean showGrid = tracks.get(0).component3();
-            Boolean isLinear = tracks.get(0).component4();
-            int verticalDivCount = tracks.get(0).component5();
-            int horizontalDivHeight = tracks.get(0).component6();
             set1.setDrawValues(true);
             set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
             set1.setCubicIntensity(0.2f);
 
             LineData data = new LineData(set1);
 
-            mCharts[i].getDescription().setEnabled(false);
-            //mCharts[i].setDrawGridBackground(showGrid);
+            ConfigureChart(mCharts, data, curveIndex);
 
-            mCharts[i].setData(data);
-
-            mCharts[i].setViewPortOffsets(120f,60f,0f,0f);
-            mCharts[i].getXAxis().setAxisMinimum(-5f);
-            mCharts[i].getXAxis().setAxisMaximum(105f);
-            mCharts[i].getAxisRight().setAxisMinimum(0f);
-            mCharts[i].getAxisRight().setAxisMaximum(100f);
-            mCharts[i].setBackgroundColor(Color.DKGRAY);
-            mCharts[i].getAxisLeft().setTextColor(Color.WHITE);
-            mCharts[i].getXAxis().setTextColor(Color.WHITE);
-            mCharts[i].getLegend().setTextColor(Color.WHITE);
-            mCharts[i].getDescription().setTextColor(Color.WHITE);
-            mCharts[i].setDrawGridBackground(false);
-            mCharts[i].invalidate();
-
-
-            }
 
         }
     }
+
+    private void SetCurveStyle(LineDataSet data, String lineStyle) {
+        switch (lineStyle) {
+            case "Normal":
+                data.setLineWidth(1f);
+                break;
+            case "Dotted":
+                data.enableDashedLine(2f, 2f, 2f);
+                break;
+            case "Bold":
+                data.setLineWidth(2f);
+                break;
+        }
+    }
+
+    private void SetCurveColor(LineDataSet data, String curveColor) {
+        switch (curveColor) {
+            case "FF0000":
+                data.setColor(Color.RED);
+                break;
+            case "0000FF":
+                data.setColor(Color.BLUE);
+                break;
+            case "00FF00":
+                data.setColor(Color.GREEN);
+                break;
+        }
+    }
+
+    private void ConfigureChart(LineChart[] mCharts, LineData data, int currChart){
+        mCharts[currChart].getDescription().setEnabled(false);
+        //mCharts[currChart].setDrawGridBackground(showGrid);
+
+        mCharts[currChart].setData(data);
+
+        mCharts[currChart].setViewPortOffsets(120f, 60f, 0f, 0f);
+        mCharts[currChart].getXAxis().setAxisMinimum(-5f);
+        mCharts[currChart].getXAxis().setAxisMaximum(105f);
+        mCharts[currChart].getAxisRight().setAxisMinimum(0f);
+        mCharts[currChart].getAxisRight().setAxisMaximum(100f);
+        mCharts[currChart].setBackgroundColor(Color.DKGRAY);
+        mCharts[currChart].getAxisLeft().setTextColor(Color.WHITE);
+        mCharts[currChart].getXAxis().setTextColor(Color.WHITE);
+        mCharts[currChart].getLegend().setTextColor(Color.WHITE);
+        mCharts[currChart].getDescription().setTextColor(Color.WHITE);
+        mCharts[currChart].setDrawGridBackground(false);
+        mCharts[currChart].invalidate();
+    }
+
+    // Initalizing Depth for Different LAS files
+    private String GetDepthName(Map<String, List<Float>> lasData){
+        for(String depthOption : DEPTH_NAMES){
+            if (lasData.containsKey(depthOption)) {
+                return depthOption;
+            }
+        }
+
+        System.exit(-1);
+        return null;
+    }
+}
 
 
 
